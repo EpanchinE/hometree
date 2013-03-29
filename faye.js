20a3aeacdc73b5163f6eb472cb515d04419ba964
Sergey Gorobets <dev@gor.sg>
2013-03-29 23:35:09 +0200

    Try faye with node js

---
+++ faye.js
@@ -0,0 +1,15 @@
+var http = require('http'),
+    faye = require('faye'),
+    fs   = require('fs');
+
+var bayeux = new faye.NodeAdapter({mount: '/faye', timeout: 45});
+bayeux.listen(9090);
+
+// Handle non-Bayeux requests
+//var server = http.createServer(function(request, response) {
+//  response.writeHead(200, {'Content-Type': 'text/html'});
+//  response.end(fs.readFileSync('views/index.erb'));
+//});
+
+//server.listen(8080);
+//bayeux.attach(server);
