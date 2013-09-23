3e4507dc6372dc2af92a10f37bdfe56fc6ea23aa
EpanchinE <epanchine@gmail.com>
2013-09-23 21:45:30 +0300

    Small Refactoring

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,21 +1,40 @@
 $(function() {
-  var token = Math.random().toString(36).substring(8);
-  var interval = 50;
-  var timer;
+  var token = Math.random().toString(36).substring(8),
+      interval = 50,
+      timer,
+      fayeUrl = "//"+ document.location.hostname + ":9090/faye",
+      push = function (id, value, type) {
+        $.ajax({
+          type: "POST",
+          url: fayeUrl,
+          dataType: 'json',
+          data: "message=" + JSON.stringify({
+            "channel": "/messages/new",
+            "data": {
+              "auth_token": auth_token,
+              "id": id,
+              "value": value,
+              "type": type,
+              "token": token
+            }
+          })
+        });
+      },
+      setCurrentValue = function () {
+        this.innerText = this.previousSibling.previousSibling.value;
+      };
 
-  var faye_url = "//"+ document.location.hostname + ":9090/faye";
-
-  $.getScript(faye_url + "/client.js", function(data, textStatus, jqxhr) {
-    var faye = new Faye.Client(faye_url);
+  $("[type=range] + span").each(setCurrentValue);
+  $.getScript(fayeUrl + "/client.js", function(data, textStatus, jqxhr) {
+    var faye = new Faye.Client(fayeUrl);
 
     faye.subscribe("/messages/new", function(data) {
-      if(data.token != token){
-        var control = $("#" + data.id)[0];
+      if (data.token !== token) {
+        var control = document.getElementById(data.id);
         control.disabled = true;
-
-        if(data.type == "checkbox") {
+        if (data.type === "checkbox") {
           control.checked = data.value;
-        } else if(data.type == "range") {
+        } else if (data.type === "range") {
           control.value = data.value;
         }
         control.disabled = false;
@@ -28,19 +47,13 @@ $(function() {
   });
 
   $("[type=range]").change(function() {
-    clearTimeout(timer);
     var element = this;
+    clearTimeout(timer);
     timer = setTimeout(function(){
       push(element.id, element.value, element.type);
+      element.nextSibling.nextSibling.innerText = element.value;
     }, interval);
   });
 
-  function push(id, value, type){
-    $.ajax({
-      type: "POST",
-      url: faye_url,
-      dataType: 'json',
-      data: "message={ \"channel\": \"/messages/new\", \"data\": { \"auth_token\": \"" + auth_token + "\", \"id\": \"" + id + "\", \"value\": " + value + ", \"type\": \"" + type + "\", \"token\": \"" + token + "\" } }",
-    });
-  }
+  
 });
